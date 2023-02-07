d3cd44b7636b8ef2ee732d5ef74fa4c982a129c3
James Goppert <james.goppert@gmail.com>
2023-02-07 09:36:34 -0500

    Clean up profile/bashrc.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- dream/install/entrypoint.sh
+++ dream/install/entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-source ~/.profile
-
 if [ "$RUN_VNC" = true ] ; then
   /opt/TurboVNC/bin/vncserver -geometry 1920x1080 -name dream -xstartup /bin/openbox-session :20
 fi
 
-exec "$@"
+exec $@
--- dream/install/user_setup.sh
+++ dream/install/user_setup.sh
@@ -15,8 +15,7 @@ mkdir ~/.vnc && echo "$VNCPASSWD" | /opt/TurboVNC/bin/vncpasswd -f > ~/.vnc/pass
   chmod 600 ~/.vnc/passwd && \
   openssl req -x509 -nodes -newkey rsa:3702 -keyout ~/.vnc/x509_private.pem -out ~/.vnc/x509_cert.pem -days 3650 -subj '/CN=www.mydom.com/O=My Company Name LTD./C=US'
 
-# setup .profile, note bashrc doesn't get sourced by docker by defualt, .profile does
-cat << EOF >> ~/.profile
+cat << EOF >> ~/.bashrc
 source /opt/ros/humble/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export GZ_SIM_RESOURCE_PATH=/workdir/dream/models:/workdir/dream/worlds
