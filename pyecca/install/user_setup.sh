4ac6ceacdabbdcfb31e4a95765c05a3041f68d7e
James Goppert <james.goppert@gmail.com>
2023-02-10 17:22:55 -0500

    Remove pyecca docker.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- pyecca/install/user_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# setup .profile, note bashrc doesn't get sourced by docker by defualt, .profile does
-cat << EOF >> ~/.profile
-source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
-EOF
-
