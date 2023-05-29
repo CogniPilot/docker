373e7398d97e7471b293fe2a7d0a8d62a766470d
James Goppert <james.goppert@gmail.com>
2023-05-29 15:40:12 +0100

    Update docker file.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- dream/install/jlink.sh
+++ dream/install/jlink.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-sudo dpkg --unpack /opt/JLink_Linux_V788c_x86_64.deb
+sudo dpkg --unpack /tmp/JLink_Linux_V788c_x86_64.deb
 sudo rm /var/lib/dpkg/info/jlink.postinst -f
 sudo dpkg --configure jlink
 sudo apt install -yf
