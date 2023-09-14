2c488358f596ca8d2607aab7783f01668a839c91
James Goppert <james.goppert@gmail.com>
2023-09-14 01:30:49 -0400

    Unify install with native.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- dream/install/latex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-sudo apt-get -y update
-sudo DEBIAN_FRONTEND=noninteractive  apt-get install --no-install-recommends -y \
-	texlive-bibtex-extra \
-	texlive-latex-extra \
-	texstudio
--- dream/install/poetry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-curl -sSL https://install.python-poetry.org | sudo POETRY_HOME=/opt/poetry python3 -
-
